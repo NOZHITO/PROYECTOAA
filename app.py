3031cf3d68c1de4097bc10cb457f9ef48017c0e5
NOZHITO <nozhito2022@gmail.com>
2026-06-09 13:03:01 -0500

    cambio del login de cerrado de sesion

--- app.py
+++ app.py
@@ -82,8 +82,11 @@ else:
     st.sidebar.markdown("---")
     st.sidebar.info(f"Usuario: {email_usuario}\nRol: {rol_usuario.upper()}")
     
-    if st.sidebar.button("Cerrar sesión"):
-        supabase.auth.sign_out() # <-- Esto destruye la sesión real en Supabase
+   if st.sidebar.button("Cerrar sesión"):
+        try:
+            supabase.auth.sign_out() # <-- Esto destruye la sesión real en Supabase
+        except Exception:
+            pass
         st.session_state['user'] = None
         st.rerun()
 
